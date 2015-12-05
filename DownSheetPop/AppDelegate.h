3c6ba85071f0bead20a99b70618e80a39aad4e9e
Weblogic <weblogic@weblogicdemacbook-pro.local>
2015-12-05 16:17:18 +0800

    Initial Commit

---
+++ DownSheetPop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownSheetPop
+//
+//  Created by Weblogic on 15/12/5.
+//  Copyright © 2015年 Weblogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
